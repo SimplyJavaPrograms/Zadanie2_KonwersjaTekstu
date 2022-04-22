399676056b48286c5352ddda96ac35c3036f5fad
MaciejIwan <64867638+MaciejIwan@users.noreply.github.com>
2022-04-22 13:00:23 +0200

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+class Substring {
+    public static void main(String[] args) {
+        
+        if (args.length != 3){
+            System.out.println("Program potrzebuje 3 agrumentow. Podaj wspolczynniki w kolejnosci: tekst, limit lewy, limit prawy");
+            System.exit(1);
+        }
+
+
+    }
+}
